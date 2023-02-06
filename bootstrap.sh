4983efc8be30f0ebb0f00f55ba37cae82b0a2d0f
Gema Parreño <gema.parreno.piqueras@gmail.com>
2023-02-06 12:47:05 +0100

    update building scenario for MAC M1

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,9 @@
+## This file launchs the python script that launch , loads data .
+## See duplicates?
 #!/bin/bash
 
 # parse environment
 ARANGO_ROOT_PASSWORD=$(cat $ARANGO_ROOT_PASSWORD_FILE)
 
 # build BRON
-python3 tutorials/build_bron.py --username=root --password=${ARANGO_ROOT_PASSWORD} --ip=brondb
+python3 tutorials/build_bron.py --username=root --password=${ARANGO_ROOT_PASSWORD} --ip=brondb --no_validation
